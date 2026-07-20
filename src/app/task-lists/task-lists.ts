81e6e6bed6c16ddcfcd288a8f6ef9134c8148931
shubham4672 <shubham4672.20@gmail.com>
2026-07-21 00:25:12 +0530

    edit task and update

--- src/app/task-lists/task-lists.ts
+++ src/app/task-lists/task-lists.ts
@@ -1,28 +1,33 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { TasksService } from '../tasks.service';
 import { Task } from '../task.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-lists',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-lists.html',
   styleUrl: './task-lists.scss',
 })
 export class TaskLists {
   private taskService = inject(TasksService);
   readonly tasks = this.taskService.tasks;
+  taskId = this.taskService.editTaskId;
+  editedTask = this.taskService.editedTask;
 
   deleteTask(id: string) {
     this.taskService.removeTask(id);
   }
 
   updateTask(task: Task) {
-    this.taskService.tasks.update((tasks) =>
-      tasks.map((t) =>
-        t.id === task.id
-          ? { ...t, status: task.status === 'Incompleted' ? 'Completed' : 'Incompleted' }
-          : t,
-      ),
-    );
+    this.taskService.updateTask(task);
+  }
+
+  editTask(task: Task) {
+    this.taskService.editTask(task);
+  }
+
+  saveTask(id: string) {
+    this.taskService.saveEditedTask(id);
   }
 }
--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -4,6 +4,8 @@ import { Task } from './task.model';
 @Injectable({ providedIn: 'root' })
 export class TasksService {
   tasks = signal<Task[]>([]);
+  editTaskId = signal<string>('');
+  editedTask = signal<string>('');
 
   constructor() {
     const storedTasks = localStorage.getItem('tasks');
@@ -24,6 +26,29 @@ export class TasksService {
   public removeTask(id: string): void {
     this.tasks.update((tasks) => tasks.filter((t) => t.id != id));
     this.saveTask();
+  }
+
+  public updateTask(task: Task) {
+    this.tasks.update((tasks) =>
+      tasks.map((t) => {
+        return t.id === task.id
+          ? { ...t, status: task.status === 'Incompleted' ? 'Completed' : 'Incompleted' }
+          : t;
+      }),
+    );
+  }
+
+  public editTask(task: Task) {
+    this.editTaskId.set(task.id);
+    this.editedTask.set(task.name);
+  }
+
+  public saveEditedTask(id: string) {
+    this.tasks.update((tasks) =>
+      tasks.map((task) => (task.id === id ? { ...task, name: this.editedTask() } : task)),
+    );
+    this.saveTask();
+    this.editTaskId.set('');
   }
 
   private saveTask() {
